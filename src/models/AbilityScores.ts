0d88c12b3ed14b87945487b35a73f4dd9340d34d
Ayush Nene <ayush3108@gmail.com>
2020-08-04 16:16:10 -0400

    add ability scores class

---
+++ src/models/AbilityScores.ts
@@ -0,0 +1,49 @@
+import { Type } from "class-transformer";
+
+export class AbilityScore {
+    score: number;
+    constructor() {
+        this.score = 8;
+    }
+    get modifier() {
+        return Math.floor((this.score - 10) / 2);
+    }
+}
+
+export class AbilityScores {
+    @Type(() => AbilityScore)
+    str: AbilityScore;
+    @Type(() => AbilityScore)
+    dex: AbilityScore;
+    @Type(() => AbilityScore)
+    con: AbilityScore;
+    @Type(() => AbilityScore)
+    int: AbilityScore;
+    @Type(() => AbilityScore)
+    wis: AbilityScore;
+    @Type(() => AbilityScore)
+    cha: AbilityScore;
+
+    constructor() {
+        this.str = new AbilityScore();
+        this.dex = new AbilityScore();
+        this.con = new AbilityScore();
+        this.int = new AbilityScore();
+        this.wis = new AbilityScore();
+        this.cha = new AbilityScore();
+    }
+
+    /**
+     * Represent as an array of tuples [stat, score]
+     */
+    getArrayForm(): [string, AbilityScore][] {
+        return [
+            ["str", this.str],
+            ["dex", this.dex],
+            ["con", this.con],
+            ["int", this.int],
+            ["wis", this.wis],
+            ["cha", this.cha],
+        ];
+    }
+}
